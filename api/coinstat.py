25be72de50aca32104af8d4771eebe5e422670fb
Kyle Ngo <kyle@Kyles-MacBook-Pro.local>
2021-03-08 00:45:54 -0800

    updated

--- api/coinstat.py
+++ api/coinstat.py
@@ -11,7 +11,17 @@ class Coinstats():
         r = requests.get(url)
         bsObj = BeautifulSoup(r.text, 'html.parser')
         totalAsset = bsObj.find('span', {'class': 'main-price'})['title']
-        return "Total: " + totalAsset
+
+        response = "Total: " + totalAsset + "\n"
+
+        coins = bsObj.find('div', {'class': 'coins-container'}).ul
+
+        for coin in coins:
+            currency = coin.find('span', {'class': 'table-row'}).text
+            amount = coin.find('span', {'class': 'primary-nav'}).text
+            response = response + currency + ": " +  amount + "\n"
+
+        return response
 
     def saveUser(self, user, link):
         self.removeUserData(user)
