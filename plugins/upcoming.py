582d24ee378ab06bd32d1e2c40161cd4f2515661
Kyle Ngo <kyle@Kyles-MacBook-Pro.local>
2021-03-08 11:18:25 -0800

    upcoming

---
+++ plugins/upcoming.py
@@ -0,0 +1,35 @@
+from telegram import ParseMode
+from plugin import PluginImpl, Keyword
+from api.upcoming import Upcoming as News
+
+class Upcoming(PluginImpl):
+    def get_cmds(self):
+        return ["upcoming"]
+
+    @PluginImpl.send_typing
+    def get_action(self, update, context):
+        if len(context.args) != 1:
+            update.message.reply_text(  
+                text=f"Usage:\n{self.get_usage()}",
+                parse_mode=ParseMode.MARKDOWN)
+            return
+        self.getEvents(update, context.args[0])
+
+    def _getMarkdown(self, response):
+        if response is None:
+            return response
+        response = response.replace(".", "\\.").replace("-", "\\-").replace("|", "\\|")
+        response = response.replace(")", "\\)").replace("(","\\(")
+        return response
+
+
+    def getEvents(self, update, ticker):
+        try:
+            response = News().getEvents(ticker)
+            update.message.bot.send_message(chat_id = update.effective_chat.id,
+            text=self._getMarkdown(response), parse_mode=ParseMode.MARKDOWN_V2)
+
+        except Exception as e:
+            print(e)
+            #return self.handle_error(f"Error. Invalid symbol {context.args[0].upper()} ", update)
+
