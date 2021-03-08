df5d83ccb50012d0603878c7acc06a128400187f
Kyle Ngo <kyle@Kyles-MacBook-Pro.local>
2021-03-07 21:23:19 -0800

    update asset

--- api/coinstat.py
+++ api/coinstat.py
@@ -7,9 +7,37 @@ class Coinstats():
         print("fuck thuan")
 
     def getAsset(self, user):
-        if(user == '@b0sssie'):
-            url = 'https://coinstats.app/p/LQZZXg'
-            r = requests.get(url)
-            bsObj = BeautifulSoup(r.text, 'html.parser')
-            totalAsset = bsObj.find('span', {'class': 'main-price'})['title']
-            return "Total: " + totalAsset
+        url = self.findUserData(user)
+        r = requests.get(url)
+        bsObj = BeautifulSoup(r.text, 'html.parser')
+        totalAsset = bsObj.find('span', {'class': 'main-price'})['title']
+        return "Total: " + totalAsset
+
+    def saveUser(self, user, link):
+        self.removeUserData(user)
+        with open("./config/coinstats.txt", "a") as myfile:
+            myfile.write(user + ' ' + link+'\n')
+
+    def findUserData(self, user):
+        try:
+            with open("./config/coinstats.txt") as fp:
+                for line in fp:
+                    line = line.strip("\n")
+                    line = line.split()
+                    if line[0] == user:
+                        return line[1]
+        except Exception as e:
+            print(e)
+
+    def removeUserData(self, user):
+        a_file = open("./config/coinstats.txt", "r")
+        lines = a_file.readlines()
+        a_file.close()
+
+        new_file = open("./config/coinstats.txt", "w")
+        for line in lines:
+            if line.split()[0] != user:
+                new_file.write(line)
+        new_file.close()
+
+
