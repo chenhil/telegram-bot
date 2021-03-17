3517e463c6c49c0806f7885482f1afd36f51f491
Van Tran <vanctran@users.noreply.github.com>
2021-03-16 20:04:42 -0700

    Output pre-market price during pre-market hours (#33)

--- plugins/stockprice.py
+++ plugins/stockprice.py
@@ -4,8 +4,19 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackQueryHandler
 from telegram.utils.helpers import escape_markdown
 import logging
+
+from datetime import datetime, time
+from pytz import timezone
+
 from api.stocksg import YahooStocksG
 from api.count import count
+
+# US_MARKET times in seconds
+US_EASTERN_TIMEZONE = timezone('US/Eastern')
+PRE_MARKET_START = 28800
+REGULAR_MARKET_START = 48600
+POST_MARKET_START = 72000
+MARKET_END = 86400
 
 class Stockprice(PluginImpl):
 
@@ -117,62 +128,48 @@ class Stockprice(PluginImpl):
 
     def _get_company_profile_markdown(self, stock_data):
         output = (str('```')) + "\n" \
-        + self._format_text("Symbol: ", self._get_if_exist(stock_data, ['symbol'])) + "\n" \
-        + self._format_text("Name: ", self._get_if_exist(stock_data, ['company_short_name'])) + "\n" \
-        + self._format_text("Sector: ", self._get_if_exist(stock_data, ['sector'])) + "\n" \
-        + self._format_text("Employees: ",  self._get_if_exist(stock_data, ['full_time_employees'])) + "\n" \
-        + self._format_text("City: ", self._get_if_exist(stock_data, ['city'])) + "\n" \
-        + self._format_text("State: ", self._get_if_exist(stock_data, ['state'])) + "\n" \
-        + self._format_text("Country: ", self._get_if_exist(stock_data, ['country'])) + "\n" \
-        + self._format_text("Website: ", self._get_if_exist(stock_data, ['website'])) + "\n" \
-        + (str('```'))
+            + self._format_text("Symbol: ", self._get_if_exist(stock_data, ['symbol'])) + "\n" \
+            + self._format_text("Name: ", self._get_if_exist(stock_data, ['company_short_name'])) + "\n" \
+            + self._format_text("Sector: ", self._get_if_exist(stock_data, ['sector'])) + "\n" \
+            + self._format_text("Employees: ",  self._get_if_exist(stock_data, ['full_time_employees'])) + "\n" \
+            + self._format_text("City: ", self._get_if_exist(stock_data, ['city'])) + "\n" \
+            + self._format_text("State: ", self._get_if_exist(stock_data, ['state'])) + "\n" \
+            + self._format_text("Country: ", self._get_if_exist(stock_data, ['country'])) + "\n" \
+            + self._format_text("Website: ", self._get_if_exist(stock_data, ['website'])) + "\n" \
+            + (str('```'))
         
         return output.replace(".", "\.").replace("-", "\-").replace("|", "\|")
 
     def _getStockPriceMarkdown(self, stock_data):
         output = ""
+        current_time = get_current_time()
         try:
             output = (str('```')) + "\n" + self._getSymbol(stock_data) + "\n" \
-            + "Market Prices" + "\n" \
-            + self._getPriceRegularHours(stock_data) + "\n"
+                + "Market Prices" + "\n"
+            
+            if is_pre_market_hours(current_time):
+                output += self._getPricePreMarketHours(stock_data) + "\n" \
+                    + "Yesterday's Prices " + "\n"
+            
+            output += self._getPriceRegularHours(stock_data) + "\n"
 
-            if 'has_post_market_data' in stock_data:
-                output += self._getPriceAfterHours(stock_data) + "\n"
+            if is_post_market_hours(current_time):
+                output += self._getPriceAfterHours(stock_data) + "\n\n"
     
             output += self._getOpen(stock_data) + "\n" \
-            + self._getPriceHigh(stock_data) + "\n" \
-            + self._getPriceLow(stock_data) + "\n" \
-            + self._getVolume(stock_data) + "\n" \
-            + self._getAvgVolume(stock_data) + "\n" \
-            + self._get52WKHigh(stock_data) + "\n" \
-            + self._get52WKLow(stock_data) + "\n" + (str('```'))
+                + self._getPriceHigh(stock_data) + "\n" \
+                + self._getPriceLow(stock_data) + "\n" \
+                + self._getVolume(stock_data) + "\n" \
+                + self._getAvgVolume(stock_data) + "\n\n" \
+                + self._get52WKHigh(stock_data) + "\n" \
+                + self._get52WKLow(stock_data) + "\n" + (str('```'))
         except Exception as err:
             print(err)
 
         return output.replace(".", "\\.").replace("-", "\\-").replace("|", "\\|")
-
-    def _get_if_exist(self, map, keyList):
-        obj = map
-        for key in keyList:
-            if key in obj:
-                obj = obj[key]
-            else:
-                return ""
-        return obj
-
-    def _format_text(self, label, text):
-        return "{0:<10} {1:<10}".format(label, text)
-
+    
     def _getSymbol(self, stock_data):
         return "{0:<10} {1:<10}".format("Symbol: ", stock_data['symbol'])
-
-    def _get_change_arrow_emoji(self, stock_data, key):
-        arrow = ""
-        if "-" in str(stock_data[key]):
-            arrow = emo.DOWN_ARROW
-        else:
-            arrow = emo.UP_ARROW
-        return arrow
 
     def _getPrice(self, stock_data, market_hours):
         price_key = market_hours + '_price'
@@ -182,11 +179,23 @@ class Stockprice(PluginImpl):
         + " \(" + stock_data[price_percent_key] + "\)"
         return "{0:>8} {1:<4}".format(price_string, arrow)
 
+    def _get_change_arrow_emoji(self, stock_data, key):
+        arrow = ""
+        if "-" in str(stock_data[key]):
+            arrow = emo.DOWN_ARROW
+        else:
+            arrow = emo.UP_ARROW
+        return arrow
+
+    def _getPricePreMarketHours(self, stock_data):
+        if stock_data['has_pre_market_data'] == True:
+            return "{0:<10} {1:<10}".format("Pre: ", self._getPrice(stock_data, 'pre_market'))
+
     def _getPriceRegularHours(self, stock_data):
         return "{0:<10} {1:<10}".format("Regular: ", self._getPrice(stock_data, 'regular_market'))
 
     def _getPriceAfterHours(self, stock_data):
-        if stock_data['has_post_market_data'] is True:
+        if stock_data['has_post_market_data'] == True:
             return "{0:<10} {1:<10}".format("After: ", self._getPrice(stock_data, 'post_market'))
         return ""
 
@@ -210,3 +219,40 @@ class Stockprice(PluginImpl):
 
     def _get52WKLow(self, stock_data):
         return "{0:<10} {1:<10}".format('52WK Low:', stock_data['fifty_two_week_low'])
+
+    def _get_if_exist(self, map, keyList):
+        obj = map
+        for key in keyList:
+            if key in obj:
+                obj = obj[key]
+            else:
+                return ""
+        return obj
+
+    def _format_text(self, label, text):
+        return "{0:<10} {1:<10}".format(label, text)
+
+def get_current_time():
+    return datetime.now(tz=US_EASTERN_TIMEZONE)
+
+def get_day_start_seconds(current_time):
+    day_start = current_time.replace(hour=0, second=0, microsecond=0, tzinfo=US_EASTERN_TIMEZONE)
+    return int(day_start.timestamp())
+
+def is_pre_market_hours(current_time):
+    day_start = get_day_start_seconds(current_time)
+    current_time_seconds = int(current_time.timestamp())
+    return is_between_interval(day_start + PRE_MARKET_START, day_start + REGULAR_MARKET_START, current_time_seconds)
+
+def is_post_market_hours(current_time):
+    day_start = get_day_start_seconds(current_time)
+    current_time_seconds = int(current_time.timestamp())
+    return is_between_interval(day_start + POST_MARKET_START, day_start + PRE_MARKET_START, current_time_seconds)
+
+# Non-inclusive [ intervalStart , intervalEnd ) interval checking
+# If a time to check is not given, this defaults to the current time.
+def is_between_interval(interval_begin, interval_end, check_time):
+    if interval_begin < interval_end:
+        return check_time >= interval_begin and check_time < interval_end
+    else: # crosses midnight
+        return check_time >= interval_begin or check_time < interval_end
