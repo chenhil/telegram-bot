c09bdcb53de4bc15c7541bddff5fbe0ab4b52858
Kyle Ngo <kyle@Kyles-MacBook-Pro.local>
2021-03-21 13:11:32 -0700

    donator

--- plugins/donator.py
+++ plugins/donator.py
@@ -7,7 +7,7 @@ class Donator(PluginImpl):
 
     @PluginImpl.send_typing
     def get_action(self, update, context):
-        response = "cashapp: $b0ssie\n1. @icahnn  - $35 cashapp\n2. @ScrubMasterAsh - $30 cashapp\n3. @i123sb - $25 bnb\n4. @Karan - $1 cashapp"
+        response = "cashapp: $b0ssie\n1. @icahnn  - $35 cashapp\n2. @ScrubMasterAsh - $30 cashapp\n3. @i123sb - $25 bnb\n4. @@GQvisions - $5 cashapp\n5. @Karan - $1 cashapp\n"
         print(response)
 
         try:
