70dbab424cc3c495d62ec73159a6741a578c4785
Duy Pham <duypham@Duys-MacBook-Pro.local>
2021-03-26 10:55:28 -0700

    added donate addresses

--- plugins/donate.py
+++ plugins/donate.py
@@ -2,21 +2,18 @@ from telegram import ParseMode
 from plugin import PluginImpl, Keyword
 
 
-class Donator(PluginImpl):
+class Donate(PluginImpl):
     def get_cmds(self):
-        return ["donator", "list", "top"]
+        return ["d", "donate"]
 
     @PluginImpl.send_typing
     def get_action(self, update, context):
         response = """
-        Donation List
-        \n1. @i123sb - $75 cashapp
-        \n2. @ScrubMasterAsh - $36 cashapp
-        \n3. @icahnn  - $35 cashapp
-        \n4. @GQvisions - $15 cashapp
-        \n5. @Karan - $1 cashapp
+        Please consider donating to support the development and upkeep of this bot.
+        \ncashapp: $b0ssie
+        \nBNB: 0xF6c07cE91e44f8A9E88Af880BEFd3bcf2b492F22
+        \nEth: 0xcc5BC3Fe73e8CD00B30C889f6b685a3de1D313ce
         """
-        print(response)
 
         try:
             update.message.bot.send_message(chat_id=update.effective_chat.id,
