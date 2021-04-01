5dc8b8d86a5bd89a0ef2031f5b07d17b91a30e45
Kyle Ngo <kyle@Kyles-MacBook-Pro.local>
2021-03-31 19:26:51 -0700

    new feature: /note

---
+++ plugins/note.py
@@ -0,0 +1,30 @@
+from telegram import ParseMode
+from plugin import PluginImpl, Keyword
+from api.coinstat import Coinstats
+from api.count import count
+import requests
+from bs4 import BeautifulSoup
+
+class Note(PluginImpl):
+    def get_cmds(self):
+        return ["note", "n"]
+
+    @PluginImpl.send_typing
+    def get_action(self, update, context):
+        url = 'http://www.itextpad.com/nvzVwBUJ0T'
+        headers = {'User-Agent': 'My User Agent 1.0'}
+        r = requests.get(url, headers=headers)
+        bsObj = BeautifulSoup(r.text, 'html.parser')
+        notes = bsObj.find('textarea', {'name':'padText'}).text
+        update.message.bot.send_message(chat_id = update.effective_chat.id,
+            text=self._getMarkdown(notes), parse_mode=ParseMode.MARKDOWN_V2)
+
+    def _getMarkdown(self, response):
+        if response is None:
+            return response
+        response = response.replace(".", "\\.").replace("-", "\\-").replace("|", "\\|")
+        response = response.replace("(", "\\(").replace(")", "\\)").replace("!", "\\!")
+        response = response.replace("@", "\\@").replace("#", "\\#").replace("$", "\\!")
+        response = response.replace("%", "\\%").replace("*", "\\*").replace("-", "\\-")
+        response = response.replace("+", "\\+").replace("[", "\\[").replace("]", "\\]")
+        return response
