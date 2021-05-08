2b2ea783c989a2291c4185bb42048f66c0fcb860
Hillman Chen <hillman@Hillmans-MacBook-Pro.local>
2021-05-07 20:07:25 -0700

    fixed plex

--- api/plex.py
+++ api/plex.py
@@ -10,7 +10,7 @@ class PlexAPI():
 
     def getRecentlyAdded(self):
         for item in self.plex.library.section("TV Shows").recentlyAdded()[:5]:
-            self.shows.append("• " + item.grandparentTitle + " " +item.seasonEpisode)
+            self.shows.append("• " + item.episodes()[-1].grandparentTitle + " " +item.episodes()[-1].seasonEpisode)
 
         for item in self.plex.library.section("Movies").recentlyAdded()[:5]:
             self.movies.append("• " + item.title)
